d0461f985371aae80cfeac790b1a218c5fd81a87
ZAPPAGREAT <zappagreat@yahoo.co.jp>
2025-09-11 11:02:31 +0900

    ストーリー更新

--- contents.php
+++ contents.php
@@ -83,6 +83,7 @@
           <a href="stories.php?id=34" >2-4</a>
           <a href="stories.php?id=35" >2-5</a>
           <a href="stories.php?id=36" >2-6</a>
+          <a href="stories.php?id=37" >2-7</a>
         </div>
       </div>
     </div>
